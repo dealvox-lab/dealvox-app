8fa6c69a8aa2565d057636cfbbf51194c66e4a10
dealvox-lab <ongo.promotion@gmail.com>
2025-11-17 09:32:05 +0300

    Update logout.js

--- functions/logout.js
+++ functions/logout.js
@@ -1,14 +1,21 @@
 // /functions/logout.js
 export const onRequestGet = async () => {
-  const headers = new Headers({ "Content-Type": "text/plain" });
+  const headers = new Headers();
   const expired = "Max-Age=0; Path=/; Secure; HttpOnly; SameSite=Lax";
 
+  // Clear all relevant cookies
   headers.append("Set-Cookie", `sb_token=; ${expired}`);
+  headers.append("Set-Cookie", `sb:token=; ${expired}`);
+  headers.append("Set-Cookie", `sb-refresh=; ${expired}`);
   headers.append("Set-Cookie", `sb_refresh=; ${expired}`);
+  headers.append("Set-Cookie", `sb:refresh=; ${expired}`);
   headers.append("Set-Cookie", `sb_dbg=; Max-Age=0; Path=/; Secure; SameSite=Lax`);
 
-  return new Response("Logged out", {
+  // Redirect to /login
+  headers.set("Location", "/login");
+
+  return new Response(null, {
     status: 302,
-    headers: { ...Object.fromEntries(headers), Location: "/login" }
+    headers,
   });
 };
